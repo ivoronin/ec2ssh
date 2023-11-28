24c3e7accca74d715954cab2bfd3824e0bb06682
Ilya Voronin <ivoronin@gmail.com>
2023-11-29 00:40:00 +0300

    Moved ec2-related functions to their own file

---
+++ ec2.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2instanceconnect"
+)
+
+var (
+	ec2Client                *ec2.EC2
+	ec2InstanceConnectClient *ec2instanceconnect.EC2InstanceConnect
+)
+
+func init() {
+	var config aws.Config
+	region := os.Getenv("AWS_DEFAULT_REGION")
+	if region != "" {
+		config.Region = aws.String(region)
+	}
+	sess := session.Must(session.NewSession(&config))
+	ec2Client = ec2.New(sess)
+	ec2InstanceConnectClient = ec2instanceconnect.New(sess)
+}
+
+func sendSSHPublicKey(instanceID, instanceOSUser, sshPublicKeyPath string) {
+	file, err := os.Open(sshPublicKeyPath)
+	if err != nil {
+		handleError(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	sshPublicKey := scanner.Text()
+	if err := scanner.Err(); err != nil {
+		handleError(err)
+	}
+
+	input := &ec2instanceconnect.SendSSHPublicKeyInput{
+		InstanceId:     aws.String(instanceID),
+		InstanceOSUser: aws.String(instanceOSUser),
+		SSHPublicKey:   aws.String(sshPublicKey),
+	}
+
+	_, err = ec2InstanceConnectClient.SendSSHPublicKey(input)
+	if err != nil {
+		handleError(err)
+	}
+}
+
+func getInstanceIPByID(instanceID string, usePublicIP bool) string {
+	input := &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{aws.String(instanceID)},
+	}
+
+	result, err := ec2Client.DescribeInstances(input)
+	if err != nil {
+		handleError(err)
+	}
+
+	for _, reservation := range result.Reservations {
+		for _, instance := range reservation.Instances {
+			if usePublicIP {
+				if instance.PublicIpAddress == nil {
+					handleError(fmt.Errorf("public IP address not found for instance with ID %s", instanceID))
+				}
+				return *instance.PublicIpAddress
+			} else {
+				if instance.PrivateIpAddress == nil {
+					handleError(fmt.Errorf("private IP address not found for instance with ID %s", instanceID))
+				}
+				return *instance.PrivateIpAddress
+			}
+		}
+	}
+
+	handleError(fmt.Errorf("no IP found for instance ID %s", instanceID))
+	return ""
+}
+
+func getInstanceIDByFilter(filterName, filterValue string) string {
+	input := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String(filterName),
+				Values: []*string{aws.String(filterValue)},
+			},
+		},
+	}
+
+	result, err := ec2Client.DescribeInstances(input)
+	if err != nil {
+		handleError(err)
+	}
+
+	for _, reservation := range result.Reservations {
+		for _, instance := range reservation.Instances {
+			return *instance.InstanceId
+		}
+	}
+
+	handleError(fmt.Errorf("no instance found with %s=%s", filterName, filterValue))
+	return ""
+}
+
+func guessDestinationType(dst string) DstType {
+	if strings.HasPrefix(dst, "i-") {
+		return DstTypeID
+	}
+
+	if strings.HasPrefix(dst, "ip-") {
+		return DstTypePrivateDNSName
+	}
+
+	ip := net.ParseIP(dst)
+	if ip != nil {
+		if ip.IsPrivate() {
+			return DstTypePrivateIP
+		} else {
+			return DstTypePublicIP
+		}
+	}
+
+	return DstTypeNameTag
+}
--- main.go
+++ main.go
@@ -1,136 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"net"
 	"os"
 	"os/exec"
-	"strings"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/service/ec2instanceconnect"
 )
-
-var (
-	ec2Client                *ec2.EC2
-	ec2InstanceConnectClient *ec2instanceconnect.EC2InstanceConnect
-)
-
-func init() {
-	var config aws.Config
-	region := os.Getenv("AWS_DEFAULT_REGION")
-	if region != "" {
-		config.Region = aws.String(region)
-	}
-	sess := session.Must(session.NewSession(&config))
-	ec2Client = ec2.New(sess)
-	ec2InstanceConnectClient = ec2instanceconnect.New(sess)
-}
-
-func sendSSHPublicKey(instanceID, instanceOSUser, sshPublicKeyPath string) {
-	file, err := os.Open(sshPublicKeyPath)
-	if err != nil {
-		handleError(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	sshPublicKey := scanner.Text()
-	if err := scanner.Err(); err != nil {
-		handleError(err)
-	}
-
-	input := &ec2instanceconnect.SendSSHPublicKeyInput{
-		InstanceId:     aws.String(instanceID),
-		InstanceOSUser: aws.String(instanceOSUser),
-		SSHPublicKey:   aws.String(sshPublicKey),
-	}
-
-	_, err = ec2InstanceConnectClient.SendSSHPublicKey(input)
-	if err != nil {
-		handleError(err)
-	}
-}
-
-func getEC2InstanceIPByID(instanceID string, usePublicIP bool) string {
-	input := &ec2.DescribeInstancesInput{
-		InstanceIds: []*string{aws.String(instanceID)},
-	}
-
-	result, err := ec2Client.DescribeInstances(input)
-	if err != nil {
-		handleError(err)
-	}
-
-	for _, reservation := range result.Reservations {
-		for _, instance := range reservation.Instances {
-			if usePublicIP {
-				if instance.PublicIpAddress == nil {
-					handleError(fmt.Errorf("public IP address not found for instance with ID %s", instanceID))
-				}
-				return *instance.PublicIpAddress
-			} else {
-				if instance.PrivateIpAddress == nil {
-					handleError(fmt.Errorf("private IP address not found for instance with ID %s", instanceID))
-				}
-				return *instance.PrivateIpAddress
-			}
-		}
-	}
-
-	handleError(fmt.Errorf("no IP found for instance ID %s", instanceID))
-	return ""
-}
-
-func getEC2InstanceIDByFilter(filterName, filterValue string) string {
-	input := &ec2.DescribeInstancesInput{
-		Filters: []*ec2.Filter{
-			{
-				Name:   aws.String(filterName),
-				Values: []*string{aws.String(filterValue)},
-			},
-		},
-	}
-
-	result, err := ec2Client.DescribeInstances(input)
-	if err != nil {
-		handleError(err)
-	}
-
-	for _, reservation := range result.Reservations {
-		for _, instance := range reservation.Instances {
-			return *instance.InstanceId
-		}
-	}
-
-	handleError(fmt.Errorf("no instance found with %s=%s", filterName, filterValue))
-	return ""
-}
-
-func guessDestinationType(dst string) DstType {
-	if strings.HasPrefix(dst, "i-") {
-		return DstTypeID
-	}
-
-	if strings.HasPrefix(dst, "ip-") {
-		return DstTypePrivateDNSName
-	}
-
-	ip := net.ParseIP(dst)
-	if ip != nil {
-		if ip.IsPrivate() {
-			return DstTypePrivateIP
-		} else {
-			return DstTypePublicIP
-		}
-	}
-
-	return DstTypeNameTag
-}
 
 func handleError(err error) {
 	fmt.Fprintf(os.Stderr, "ec2ssh: error: %v\n", err)
@@ -161,17 +35,17 @@ func main() {
 	case DstTypeID:
 		instanceID = sshArgs.Destination()
 	case DstTypePrivateIP:
-		instanceID = getEC2InstanceIDByFilter("private-ip-address", sshArgs.Destination())
+		instanceID = getInstanceIDByFilter("private-ip-address", sshArgs.Destination())
 	case DstTypePublicIP:
-		instanceID = getEC2InstanceIDByFilter("ip-address", sshArgs.Destination())
+		instanceID = getInstanceIDByFilter("ip-address", sshArgs.Destination())
 	case DstTypePrivateDNSName:
-		instanceID = getEC2InstanceIDByFilter("private-dns-name", sshArgs.Destination()+".*")
+		instanceID = getInstanceIDByFilter("private-dns-name", sshArgs.Destination()+".*")
 	case DstTypeNameTag:
-		instanceID = getEC2InstanceIDByFilter("tag:Name", sshArgs.Destination())
+		instanceID = getInstanceIDByFilter("tag:Name", sshArgs.Destination())
 	}
 
 	if dstType != DstTypePrivateIP && dstType != DstTypePublicIP {
-		ip := getEC2InstanceIPByID(instanceID, opts.usePublicIP)
+		ip := getInstanceIPByID(instanceID, opts.usePublicIP)
 		sshArgs.SetDestination(ip)
 	} else if opts.usePublicIP {
 		handleWarning("the option '--use-public-ip' is ignored since an IP address has been provided")
