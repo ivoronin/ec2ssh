94f313ede7d58bb1452f2d53fc49915f22fd759e
Ilya Voronin <ivoronin@gmail.com>
2024-03-24 11:18:44 +0100

    Added tests for *.compute.internal hostname detection

--- util_test.go
+++ util_test.go
@@ -13,6 +13,8 @@ func TestGuessAWSDestinationType(t *testing.T) {
 	assert.Equal(t, DstTypePublicIP, GuessDestinationType("1.1.1.1"))
 	assert.Equal(t, DstTypeID, GuessDestinationType("i-1234567890abcdef0"))
 	assert.Equal(t, DstTypePrivateDNSName, GuessDestinationType("ip-192-168-1-1"))
+	assert.Equal(t, DstTypePrivateDNSName, GuessDestinationType("ip-192-168-1-1.compute.internal"))
+	assert.Equal(t, DstTypePrivateDNSName, GuessDestinationType("i-1234567890abcdef0.compute.internal"))
 	assert.Equal(t, DstTypeNameTag, GuessDestinationType("test"))
 	assert.Equal(t, DstTypeIPv6, GuessDestinationType("fec1::1"))
 	assert.Equal(t, DstTypeNameTag, GuessDestinationType("[fec1::1]"))
