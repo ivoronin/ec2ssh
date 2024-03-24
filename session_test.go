9714456f3c4be3b3a89641bda524829646003dee
Ilya Voronin <ivoronin@gmail.com>
2024-03-24 13:49:27 +0100

    Added session and sshkey tests

---
+++ session_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildSSHArgs(t *testing.T) {
+	t.Parallel()
+
+	session := &Session{
+		options: Options{
+			Login: "login",
+			Port:  "2222",
+		},
+		instance: types.Instance{
+			InstanceId: aws.String("instance-id"),
+		},
+		destinationAddr: "192.168.0.1",
+		privateKeyPath:  "/path/to/private/key",
+	}
+
+	sshArgs := session.buildSSHArgs()
+	assert.Equal(t, []string{
+		"-llogin",
+		"-p2222",
+		"-i/path/to/private/key",
+		"-oHostKeyAlias=instance-id",
+		"192.168.0.1",
+	}, sshArgs)
+}
